b1699fb4ff036ff197acd1837294ee224a583b4c
jiyu8iighx <jiyu8iighx@outlook.com>
2021-07-10 17:13:13 +0000

    extension in conf; expression rename; debug

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,8 @@ author = 'jiyu8iighx'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'sphinx.ext.mathjax'
+	'sphinx.ext.mathjax',
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
